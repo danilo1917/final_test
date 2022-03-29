efb86e7ec66ace75ba3ec13f00a0d7423711f93a
Lucas <lucasmoraes7551@gmail.com>
2022-03-29 13:44:25 -0300

    Add consumer model and task for the discount rule model

--- core/models.py
+++ core/models.py
@@ -2,3 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+
+class Consumer(models.Model):
+    name = models.CharField("Nome do Consumidor", max_length=128)
+    document = models.CharField("Documento(CPF/CNPJ)", max_length=14, unique=True)
+    city = models.CharField("Cidade", max_length=128)
+    state = models.CharField("Estado", max_length=128)
+    consumption = models.IntegerField("Consumo(kWh)")
+    #  create the foreign key for discount rule model here
+
+
+# TODO: Create the model DiscountRules below
+"""Fields:
+-> Consumer type  
+-> Consumption range
+-> Cover
+-> discount value
+The first three fields should be a select with the values provided in the table
+defined in the readme of the repository. Discount should be numerical
+"""
